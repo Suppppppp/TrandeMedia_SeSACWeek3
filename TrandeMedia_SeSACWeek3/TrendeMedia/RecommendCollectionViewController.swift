28de30ee4682121b2cbc2cce99ac911c600a7383
Suppppppp <u1316@daum.net>
2022-07-20 12:29:43 +0900

    add collection view func

--- TrandeMedia_SeSACWeek3/TrendeMedia/RecommendCollectionViewController.swift
+++ TrandeMedia_SeSACWeek3/TrendeMedia/RecommendCollectionViewController.swift
@@ -18,6 +18,16 @@ class RecommendCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // collection view가 화면에 보여지기 전에 셀크기 셀사이간격등 설정
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 4)
+        layout.itemSize = CGSize(width: width / 3, height: (width / 3) * 1.2)
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing  = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
         
     }
     
